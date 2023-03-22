27685d8d2a1c68bea0dd777681e08956b8b087b4
Sloth73 <Pichl.tomas.95@gmail.com>
2023-03-22 21:22:44 +0100

    Current score variable

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 'use strict';
 
 //DOM manipulation
-const currentScoreP1 = document.querySelector('#score--0');
-const currentScoreP2 = document.querySelector('#score--1');
+const ScoreP1 = document.querySelector('#score--0');
+const ScoreP2 = document.querySelector('#score--1');
 const diceImg = document.querySelector('.dice');
 const btnRoll = document.querySelector('.btn--roll');
 const btnNew = document.querySelector(".btn--new");
@@ -13,13 +13,17 @@ currentScoreP1.textContent = 0;
 currentScoreP2.textContent = 0;
 diceImg.classList.add('hidden'); //Adds a hidden class on dice Img
 
+let currentScore = 0;
+
 //Dice roll
 btnRoll.addEventListener('click', function () {
     //Random number
     let dice = Math.trunc(Math.random() * 6 + 1);
     console.log(dice)
+
     //Display dice
     diceImg.classList.remove('hidden');
     diceImg.src = `images/dice_${dice}.png`;
-
+    
+  
 });
