70dc207485e12f9efb105765ffcd0e21926c153e
Sloth73 <Pichl.tomas.95@gmail.com>
2023-03-22 20:51:01 +0100

    Selection elements from html

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+//DOM manipulation
+const currentScoreP1 = document.querySelector('#score--0');
+const currentScoreP2 = document.querySelector('#score--1');
+const diceImg = document.querySelector('.dice');
+const btnRoll = document.querySelector('.btn--roll');
+const btnNew = document.querySelector(".btn--new");
+const btnHold = document.querySelector(".btn--hold");
+
